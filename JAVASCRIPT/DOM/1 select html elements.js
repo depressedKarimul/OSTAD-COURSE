cf4f4f71d4d8c5bc89e8431bf13a7d9c399c0f1b
depressedKarimul <karimul.hassan09@gmail.com>
2025-10-17 08:54:21 +0600

    Select Html element using DOM

---
+++ JAVASCRIPT/DOM/1 select html elements.js
@@ -0,0 +1,17 @@
+//  getElementByID
+
+// var myheading = document.getElementById("heading1");
+// myheading.innerHTML = "Hello";
+
+
+// document.getElementById("para").innerHTML = "This is Heading 2"
+
+
+// getElementByTagName
+
+document.getElementsByTagName("h1")[1].innerHTML="Heyyyy";
+
+
+// getElementsByClassName
+
+document.getElementsByClassName("para1")[1].innerHTML = "Heyy duniyaa"
