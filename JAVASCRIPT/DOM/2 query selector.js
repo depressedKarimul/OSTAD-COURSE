26808d48260c5faa4397c3fe16bbaf783e8a0ebc
depressedKarimul <karimul.hassan09@gmail.com>
2025-10-17 09:14:03 +0600

    Query Selector

---
+++ JAVASCRIPT/DOM/2 query selector.js
@@ -0,0 +1,16 @@
+// querySelector
+
+// document.querySelector("#pid").innerHTML = "This is change"
+
+// document.querySelector(".pid2").innerHTML = "This is change 2";
+
+
+// document.querySelector("p").innerHTML = "change";
+
+document.querySelectorAll("p")[0].innerHTML = "change";
+
+
+document.querySelector("li a").innerHTML= "shuvo";
+
+let link = document.querySelector(".my-div a");
+link.style.color = "red";
